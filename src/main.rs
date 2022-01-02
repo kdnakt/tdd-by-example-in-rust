3a5041dd77a54d4ec087524e78dfcd9fb1e9670d
kdnakt <a.kid.1985@gmail.com>
2022-01-02 18:00:44 +0900

    test multiplication

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_multiplication() {
+        let five = Dollar { amount: 5 };
+        five.times(2);
+        assert_eq!(10, five.amount);
+    }
+}
