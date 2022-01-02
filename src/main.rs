5ea0651f7921b6cf22a939c4b9c1f44254a960a9
kdnakt <a.kid.1985@gmail.com>
2022-01-02 18:04:04 +0900

    implement times()

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,22 @@ fn main() {
     println!("Hello, world!");
 }
 
+struct Dollar {
+    amount: i64,
+}
+
+impl Dollar {
+    fn times(&mut self, multiplier: i64) {
+        self.amount *= multiplier;
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn test_multiplication() {
-        let five = Dollar { amount: 5 };
+        let mut five = Dollar { amount: 5 };
         five.times(2);
         assert_eq!(10, five.amount);
     }
